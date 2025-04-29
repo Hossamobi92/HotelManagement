54ce360c69e2b214a9cacf00007993e7aabe4349
hossamobi92 <hossamobi882@gmail.com>
2025-04-30 00:47:57 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+
+
 public class Main {
     public static void main(String[] args) {
 
 
+        CustomerService customerService = new CustomerService();
+        RoomService roomService = new RoomService();
+        BookingSystem bookingSystem = new BookingSystem();
+
+
+        Customer kunde = new Customer("Hossam","Lange Str 60 ","017565555",1);
+        customerService.addCustomer(kunde);
+
+
+        Room zimmer = new Room(101,"einzelzimmer",75.0,"123");
+        roomService.addRoom(zimmer);
+
+
+        bookingSystem.createBooking(kunde,zimmer, LocalDate.of(2025,5,1), LocalDate.of(2025,5,5));
+
+        bookingSystem.printAllBookings();
 
 
     }
