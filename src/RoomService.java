875195eadb9e5c106b554365a2de4991936b52ca
hossamobi92 <hossamobi882@gmail.com>
2025-04-30 00:49:44 +0200

    Methode getAllRooms erstellt

--- src/RoomService.java
+++ src/RoomService.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoomService {
+    private List<Room> rooms = new ArrayList<>();
 
 
 
@@ -6,4 +10,13 @@ public class RoomService {
     public void addRoom(Room room){
 
     }
+
+
+    //Alle Zimmer anzeigen (Zimmer 101 – Einzelzimmer – 60€/Nacht).
+
+    public List<Room> getAllRooms(){
+        return rooms;
+    }
+
+
 }
